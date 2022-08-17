8e6c252a64fad8aa7adcc40b12227c3d661bf1c7
Allana Karla <allanakarla1702@gmail.com>
2022-08-17 18:59:00 -0300

    :sparkles: feat: implementa app

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,11 @@
 import express from 'express';
+import 'express-async-errors';
+import route from './routers/car.route';
+import errorHandler from './middlewares/error';
 
 const app = express();
+app.use(express.json());
+app.use(route);
+app.use(errorHandler);
 
 export default app;
