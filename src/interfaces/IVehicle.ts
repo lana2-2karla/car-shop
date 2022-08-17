7189a5821a65f33b9cbf82ba20b819779f071c47
Allana Karla <allanakarla1702@gmail.com>
2022-08-17 18:56:29 -0300

    :recycle: refactor: cria mensagens de erro

--- src/interfaces/IVehicle.ts
+++ src/interfaces/IVehicle.ts
@@ -1,11 +1,21 @@
 import { z } from 'zod';
 
 const vehicleZodSchema = z.object({
-  model: z.string().min(3),
-  year: z.number().gte(1900).lte(2022),
-  color: z.string().min(3),
+  model: z.string({ 
+    required_error: 'model is required',
+    invalid_type_error: 'model must be a string' })
+    .min(3, { message: 'model must be 3 or more characters long' }),
+  year: z.number({ 
+    required_error: 'year is required',
+    invalid_type_error: 'year must be a number' }).gte(1900).lte(2022),
+  color: z.string({ 
+    required_error: 'color is required',
+    invalid_type_error: 'color must be a string' })
+    .min(3, { message: 'color must be 3 or more characters long' }),
   status: z.boolean().optional(),
-  buyValue: z.number().int(),
+  buyValue: z.number({ 
+    required_error: 'buyValue is required',
+    invalid_type_error: 'buyValue must be a number' }).int(),
 });
 
 type IVehicle = z.infer<typeof vehicleZodSchema>;
