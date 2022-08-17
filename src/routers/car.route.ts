6d77fce0e277685492c9e7bf7b9b20685bdcd5eb
Allana Karla <allanakarla1702@gmail.com>
2022-08-17 18:57:23 -0300

    :sparkles: feat: cria rota /cars - método POST

---
+++ src/routers/car.route.ts
@@ -0,0 +1,14 @@
+import { Router } from 'express';
+import CarsController from '../controllers/car.controller';
+import Car from '../models/Car';
+import CarsService from '../services/car.service';
+
+const route = Router();
+
+const car = new Car();
+const carService = new CarsService(car);
+const carController = new CarsController(carService);
+
+route.post('/cars', (req, res) => carController.create(req, res));
+
+export default route;
