b45524e996c2fdd64b9f72f8cc8f675c862c7458
Allana Karla <allanakarla1702@gmail.com>
2022-08-18 16:15:14 -0300

    :sparkles: feat: cria rota /cars/id - método GET

--- src/routers/car.route.ts
+++ src/routers/car.route.ts
@@ -11,5 +11,6 @@ const carController = new CarsController(carService);
 
 route.post('/cars', (req, res) => carController.create(req, res));
 route.get('/cars', (req, res) => carController.read(req, res));
+route.get('/cars/:id', (req, res) => carController.readOne(req, res));
 
 export default route;
